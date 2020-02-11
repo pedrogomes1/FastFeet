b25ffa9a24cf367cca1a24aa41dbb35aaafb0b62
Pedro <pedro-gomes17@hotmail.com>
2020-02-11 15:23:56 -0300

    Arquivos de configuração para envio de e-mails

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "fd711a85d137c8",
+    pass: "129d8877dc1952"
+  },
+  default: {
+    from: "Equipe FastFeed <noreply@fastfeed.com"
+  }
+};
