52528e025cbc43c10dcb853f3099b356584e2a85
Pedro <pedro-gomes17@hotmail.com>
2020-02-11 15:16:08 -0300

    Model, migration e controller dos destinatários

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,65 @@
+import * as Yup from "yup";
+import Recipient from "../models/Recipient";
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      cep: Yup.string().required()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Validations fails" });
+    }
+    const { street, number, complement, state, city, cep } = req.body;
+
+    const newRecipient = await Recipient.create({
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep
+    });
+
+    return res.json(newRecipient);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      street: Yup.string(),
+      number: Yup.number(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      cep: Yup.string()
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ erro: "Validations fails" });
+    }
+
+    const recipient = await Recipient.findByPk(req.params.id);
+
+    if (!recipient) {
+      return res.status(401).json({ message: "Usuário não existe" });
+    }
+
+    const {
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep
+    } = await recipient.update(req.body);
+
+    return res.json({ street, number, complement, state, city, cep });
+  }
+}
+
+export default new RecipientController();
---
+++ src/database/migrations/20200211140632-recipients.js
@@ -0,0 +1,48 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("recipients", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      number: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      complement: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      state: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      cep: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable("recipients");
+  }
+};
